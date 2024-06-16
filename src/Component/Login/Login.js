c0294d36f684023819d7436de6bc2a968d2885b5
MiJeeLee <dlalwl723@naver.com>
2024-06-16 13:38:26 +0900

    로그인 화면 퍼블리싱중

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.css';
 import Ellipse41 from '../../assets/Ellipse 41.svg';
+import Ellipse42 from '../../assets/Ellipse 42.svg';
 
 
 function Login() {
@@ -9,6 +10,7 @@ function Login() {
       <div className="login_txt">자연과<br/>함께하는 여행</div>
       <div className="logo">ECO TOUR</div>
       <img src={Ellipse41} alt="Ellipse 41" className='e41'/>
+      <img src={Ellipse42} alt="Ellipse 42" className='e42'/>
     </div>
   );
 }

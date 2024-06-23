f77d3bd15dfca28879c438e5680c00a7817f97f2
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-06-23 16:43:58 +0900

    메인 페이지 이동 연결

--- src/Component/Agreement/Agreement.js
+++ src/Component/Agreement/Agreement.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import './Agreement.css';
 import Agreement_Arrow from '../../assets/agreement_arrow.svg';
 import Agreement_Check from '../../assets/agree_check.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Agreement() {
+
+  const navigate = useNavigate();
 
   const [isAllClicked, setIsAllClicked] = useState(false);
   const [isClicked1, setIsClicked1] = useState(false);
@@ -41,6 +44,17 @@ function Agreement() {
     }
   };
 
+  // 회원가입 버튼 클릭 시
+  const onClickSignUpBtn = () => {
+    if(isAllClicked == true){
+      navigate('/main');
+    }
+
+    else{
+      alert('전체 약관 동의를 해주세요');
+    }
+  }
+
   return (
     <div className="container">
       <div class="agreement_title">약관을 동의해주세요</div>
@@ -49,7 +63,7 @@ function Agreement() {
           <div className={`agree_btn ${isAllClicked ? 'clicked' : ''}`} onClick={allCheckBtnClick}>
             <img src={Agreement_Check} className={`agree_check ${isAllClicked ? 'clicked' : ''}`}></img>
           </div>
-          <div class="agree_text">약관 전체 동의</div>
+          <div className={`agree_text ${isAllClicked ? 'clicked' : ''}`}>약관 전체 동의</div>
         </div>
         <div class="agree_line"></div>
         <div>
@@ -57,25 +71,25 @@ function Agreement() {
             <div className={`agree_btn ${isClicked1 ? 'clicked' : ''}`} onClick={isCheckBtnClick1}>
               <img src={Agreement_Check} className={`agree_check ${isClicked1 ? 'clicked' : ''}`}></img>
             </div>
-            <div class="agree_text">이용약관 동의 (필수)</div>
+            <div className={`agree_text ${isClicked1 ? 'clicked' : ''}`}>이용약관 동의 (필수)</div>
             <img src={Agreement_Arrow} className='agree_arrow'/>
           </div>
           <div class="agreement_wrap">
             <div className={`agree_btn ${isClicked2 ? 'clicked' : ''}`} onClick={isCheckBtnClick2}>
               <img src={Agreement_Check} className={`agree_check ${isClicked2 ? 'clicked' : ''}`}></img>
             </div>
-            <div class="agree_text">개인정보 수집 및 이용동의 (필수)</div>
+            <div className={`agree_text ${isClicked2 ? 'clicked' : ''}`}>개인정보 수집 및 이용동의 (필수)</div>
             <img src={Agreement_Arrow} className='agree_arrow'/>
           </div>
           <div class="agreement_wrap">
             <div className={`agree_btn ${isClicked3 ? 'clicked' : ''}`} onClick={isCheckBtnClick3}>
               <img src={Agreement_Check} className={`agree_check ${isClicked3 ? 'clicked' : ''}`}></img>
             </div>
-            <div class="agree_text">만 14세 이상입니다 (필수)</div>
+            <div className={`agree_text ${isClicked3 ? 'clicked' : ''}`}>만 14세 이상입니다 (필수)</div>
             <img src={Agreement_Arrow} className='agree_arrow'/>
           </div>
         </div>
-        <div id="signUp_btn"><div id="signUp_btn_text">회원가입 완료하기</div></div>
+        <div id="signUp_btn" onClick={onClickSignUpBtn}><div id="signUp_btn_text">회원가입 완료하기</div></div>
       </div>
     </div>
   );

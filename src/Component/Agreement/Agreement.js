d6a4f8405183542c6646a08783a0f83c4a8e4a2c
jpg723 <86431761+jpg723@users.noreply.github.com>
2024-06-23 16:36:21 +0900

    체크 버튼 기능 구현

--- src/Component/Agreement/Agreement.js
+++ src/Component/Agreement/Agreement.js
@@ -6,9 +6,39 @@ import Agreement_Check from '../../assets/agree_check.svg';
 function Agreement() {
 
   const [isAllClicked, setIsAllClicked] = useState(false);
+  const [isClicked1, setIsClicked1] = useState(false);
+  const [isClicked2, setIsClicked2] = useState(false);
+  const [isClicked3, setIsClicked3] = useState(false);
 
   const allCheckBtnClick = () => {
     setIsAllClicked(!isAllClicked);
+    setIsClicked1(!isClicked1);
+    setIsClicked2(!isClicked2);
+    setIsClicked3(!isClicked3);
+  };
+
+  const isCheckBtnClick1 = () => {
+    setIsClicked1(!isClicked1);
+
+    if(isClicked2 == true && isClicked3 == true){
+      setIsAllClicked(!isAllClicked);
+    }
+  };
+
+  const isCheckBtnClick2 = () => {
+    setIsClicked2(!isClicked2);
+
+    if(isClicked1 == true && isClicked3 == true){
+      setIsAllClicked(!isAllClicked);
+    }
+  };
+
+  const isCheckBtnClick3 = () => {
+    setIsClicked3(!isClicked3);
+
+    if(isClicked1 == true && isClicked2 == true){
+      setIsAllClicked(!isAllClicked);
+    }
   };
 
   return (
@@ -17,24 +47,30 @@ function Agreement() {
       <div class="agree_box">
         <div class="agreement_wrap">
           <div className={`agree_btn ${isAllClicked ? 'clicked' : ''}`} onClick={allCheckBtnClick}>
-            <img src={Agreement_Check} className='agree_check'></img>
+            <img src={Agreement_Check} className={`agree_check ${isAllClicked ? 'clicked' : ''}`}></img>
           </div>
           <div class="agree_text">약관 전체 동의</div>
         </div>
         <div class="agree_line"></div>
         <div>
           <div class="agreement_wrap">
-            <div class="agree_btn"></div>
+            <div className={`agree_btn ${isClicked1 ? 'clicked' : ''}`} onClick={isCheckBtnClick1}>
+              <img src={Agreement_Check} className={`agree_check ${isClicked1 ? 'clicked' : ''}`}></img>
+            </div>
             <div class="agree_text">이용약관 동의 (필수)</div>
             <img src={Agreement_Arrow} className='agree_arrow'/>
           </div>
           <div class="agreement_wrap">
-            <div class="agree_btn"></div>
+            <div className={`agree_btn ${isClicked2 ? 'clicked' : ''}`} onClick={isCheckBtnClick2}>
+              <img src={Agreement_Check} className={`agree_check ${isClicked2 ? 'clicked' : ''}`}></img>
+            </div>
             <div class="agree_text">개인정보 수집 및 이용동의 (필수)</div>
             <img src={Agreement_Arrow} className='agree_arrow'/>
           </div>
           <div class="agreement_wrap">
-            <div class="agree_btn"></div>
+            <div className={`agree_btn ${isClicked3 ? 'clicked' : ''}`} onClick={isCheckBtnClick3}>
+              <img src={Agreement_Check} className={`agree_check ${isClicked3 ? 'clicked' : ''}`}></img>
+            </div>
             <div class="agree_text">만 14세 이상입니다 (필수)</div>
             <img src={Agreement_Arrow} className='agree_arrow'/>
           </div>
